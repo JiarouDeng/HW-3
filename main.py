6840edfc8e4c3b43a2a70ab634bcd8614d19ac7d
JiarouDeng <dpj5243@psu.edu>
2020-09-19 01:55:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def digit_sum(n):
+  if (n>0):
+    return int(n%10 +digit_sum(n/10));
+  else:
+    return int(n)
+
+def run():
+  n= int(input("Enter an int: "));
+  print(f"sum of digits of {n} is {digit_sum(n)}.")
+
+if __name__ == "__main__":
+  run()
